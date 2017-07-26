5577ac3584ea79906be8588a529f9618c9b9da07
Utilisateur <Utilisateur@DESKTOP-27POOSQ>
2017-07-26 23:56:18 +0200

    # WARNING: head commit changed in the meantime
    start

---
+++ ebooks/src/main/java/org/fagu/ebooks/bootstrap/UpdateAuthorBootstrap.java
@@ -0,0 +1,52 @@
+package org.fagu.ebooks.bootstrap;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.FilenameUtils;
+import org.fagu.ebooks.EBooksFile;
+
+
+/**
+ * @author fagu
+ */
+public class UpdateAuthorBootstrap {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if(args.length != 1) {
+			System.err.println("Usage: java " + UpdateAuthorBootstrap.class.getName() + " <folder>");
+			return;
+		}
+
+		File folder = new File(args[0]);
+		File[] files = folder.listFiles(f -> f.isFile() && "epub".equalsIgnoreCase(FilenameUtils.getExtension(f.getName())));
+		if(files == null) {
+			return;
+		}
+
+		Map<String, String> metadataMap = new HashMap<>();
+		metadataMap.put("creator", folder.getName());
+		metadataMap.put("publisher", "nobody");
+		metadataMap.put("contributor", "");
+
+		System.out.println("Update author with " + folder.getName());
+		System.out.println();
+
+		for(File file : files) {
+			System.out.println(file.getName() + "...");
+			try {
+				EBooksFile eBooksFile = EBooksFile.open(file);
+				File newFile = eBooksFile.writeMetadatas(metadataMap);
+				file.delete();
+				newFile.renameTo(file);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
